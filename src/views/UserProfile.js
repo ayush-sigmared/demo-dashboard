344ec52350b8ef49bb3cd6b4a67ac18047b9474d
senjuti15 <senjuti@sigmared.ai>
2020-07-18 23:32:41 +0530

    demo-dashboard

---
+++ src/views/UserProfile.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardText,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col
+} from "reactstrap";
+
+class UserProfile extends React.Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default UserProfile;
